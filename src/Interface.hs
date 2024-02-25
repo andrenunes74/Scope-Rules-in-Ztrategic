5d034895d2e500710c2a869f03e4350459187695
andrenunes74 <andrebernardonunes99@gmail.com>
2024-02-25 15:23:07 +0000

    refined interface

--- src/Interface.hs
+++ src/Interface.hs
@@ -19,10 +19,6 @@ class Typeable a => Scopes a where
     isDecl :: Typeable a => Zipper a -> Bool
     isUse :: Typeable a => Zipper a -> Bool
     isBlock :: Typeable a => Zipper a -> Bool
-    isRoot :: Typeable a => Zipper a -> Bool
-    isIts :: Typeable a => Zipper a -> Bool
-    isEndIts :: Typeable a => Zipper a -> Bool
-    isExp :: Typeable a => Zipper a -> Bool
 
 instance Scopes (C.Item) where
     isDecl ag = case (S.constructor ag) of
@@ -38,39 +34,14 @@ instance Scopes (C.Item) where
         S.CVar -> True
         S.CFuncao -> True
         _ -> False
-    isRoot ag = case (S.constructor ag) of
-        S.COpenFuncao -> True
-        S.COpenWhile -> True
-        S.COpenLet -> True
-        S.COpenIf -> True
-        _ -> False
     isBlock ag = case (S.constructor ag) of
         S.CIf -> True
         S.CWhile -> True
         S.CLet -> True
         _ -> False
-    isIts ag = case (S.constructor ag) of
-        S.CConsIts -> True
-        _ -> False
-    isEndIts ag = case (S.constructor ag) of
-        S.CNilIts -> True
-        _ -> False
-    isExp ag = case (S.constructor ag) of
-        S.CAdd -> True
-        S.CSub -> True
-        S.CDiv -> True
-        S.CMul -> True
-        S.CLess -> True
-        S.CGreater -> True
-        S.CEquals -> True
-        S.CGTE -> True
-        S.CLTE -> True
-        S.CAnd -> True
-        S.COr -> True
-        _ -> False
     
 build :: Scopes a => Zipper a -> B.P
-build a = if isRoot a then B.Root (build' $ a.$1) else B.Root B.NilIts
+build a = B.Root (build' $ a.$1)
 
 build' :: Scopes a => Zipper a -> B.Its
 build' a | isDecl a = case (S.constructor a) of
@@ -81,12 +52,23 @@ build' a | isDecl a = case (S.constructor a) of
                        S.CVar -> B.ConsIts (B.Use (S.lexeme a)) B.NilIts
                        S.CFuncao -> B.ConsIts (B.Use (S.lexeme a)) (build' $ a.$2)
          | isBlock a = B.ConsIts (B.Block (TB.mergeIts (build' $ a.$1) (build' $ a.$2))) B.NilIts
-         | isIts a = (TB.mergeIts (build' $ a.$1) (build' $ a.$2))
-         | isExp a = (TB.mergeIts (build' $ a.$1) (build' $ a.$2))
-         | isEndIts a = B.NilIts
          | otherwise = case (S.constructor a) of
                             S.CConst -> B.NilIts
                             S.CBool -> B.NilIts
-                            _ -> (build' $ a.$1)
+                            S.CNilIts -> B.NilIts
+                            _ -> buildChildren a
+
+children :: Scopes a => Zipper a -> [B.Its]
+children ag = case down' ag of 
+  Nothing -> []
+  Just n -> build' n : brothers n 
+
+brothers :: Scopes a => Zipper a -> [B.Its]
+brothers ag = case right ag of 
+ Nothing -> []
+ Just n -> build' n : brothers n 
+
+buildChildren :: Scopes a => Zipper a -> B.Its
+buildChildren ag = foldr TB.mergeIts B.NilIts (children ag) 
 
 main'' a = M.block $ build $ mkAG a
