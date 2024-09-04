c77e3576fa6fd9a501a8b3b6fe37edca99bd6d60
José Nuno Macedo <jose.n.macedo@inesctec.pt>
2024-09-04 23:01:42 +0100

    Experimental - decls are lists

--- src/Haskell_Interface.hs
+++ src/Haskell_Interface.hs
@@ -1,149 +1,154 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Haskell_Interface where 
+{-# LANGUAGE TemplateHaskell, PackageImports #-}
+module Haskell_Interface where
+
+-- import "haskell-src" Language.Haskell.Syntax
+-- import qualified "ghc" Language.Haskell.Syntax as GHC
 
 import Language.Haskell.Syntax
+
 import Language.Haskell.Parser
 import Language.Haskell.Pretty
 
+import qualified Language.Haskell.Exts as LHE
+import Library.Ztrategic
+
 import Data.Generics.Zipper
-import AGMaker.AGMaker 
+import AGMaker.AGMaker
 import IScopes
 
 -- import GeneratedAG
 import Data.Maybe (isJust)
+import Language.Grammars.ZipperAG (parent)
+import Library.ZipperAG ((.^^))
+import GHC.IO (unsafePerformIO)
 
 makeAG ''HsModule
 -- writeAG ''HsModule 
 
-instance StrategicData HsModule where 
+instance StrategicData HsModule where
     isTerminal t = isJust (getHole t :: Maybe SrcLoc)
 
-instance Scopes HsModule where 
-    isDecl t = case constructor t of 
+instance Scopes HsModule where
+    isDecl t = case constructor t of
                 CHsPVar -> True
+                -- CHsPatBind -> True -- we try to fix functions
+                CHsFunBind -> True -- we try to fix functions
                 CHsConDecl -> True -- declaration of a constructor
                 CHsRecDecl -> True -- declaration of a constructor
+                -- CHsIdent -> case constructor $ parent t of -- check if we are a name of a function
+                --                 CHsMatch -> True
+                --                 _ -> False
+                -- CHsSymbol -> case constructor $ parent t of -- check if we are a name of a function
+                --                 CHsMatch -> True
+                --                 _ -> False
                 _ -> False
-    isUse t = case constructor t of 
-                CHsVar    -> True 
+    isUse t = case constructor t of
+                CHsVar    -> True
+                -- CQual     -> True
                 CHsVarOp  -> True -- infix operator
                 CHsQVarOp -> case lexeme_HsQVarOp t of -- infix operator
-                    Special _ -> False 
-                    _         -> True  
+                    Special _ -> False
+                    _         -> True
                 CHsConOp  -> True -- infix operator
                 CHsQConOp -> case lexeme_HsQConOp t of -- infix operator
-                    Special _ -> False 
-                    _         -> True 
+                    Special _ -> False
+                    _         -> True
                 CHsPApp      -> True -- Constructor application
                 CHsPInfixApp -> True -- Constructor application, but infix
-                _ -> False   
-    isBlock t = case constructor t of 
+                _ -> False
+    isBlock t = case constructor t of
                 -- let expression
                 CHsLet          -> True
+                -- function bind
+                CHsMatch        -> True
                 -- any right-hand side. This covers functions 
                 CHsGuardedRhss  -> True
                 CHsUnGuardedRhs -> True
                 -- patterns in a case expression
                 CHsAlt          -> True
-                _ -> False  
+                _ -> False
     getUse t = case constructor t of
-                CHsVar -> case lexeme_HsVar t of 
-                    Qual _ (HsIdent s)  -> s
-                    Qual _ (HsSymbol s) -> s
-                    UnQual (HsIdent s)  -> s
-                    UnQual (HsSymbol s) -> s
-                -- here downwards is infixes
-                CHsVarOp  -> case lexeme_HsVarOp t of 
-                    HsIdent s  -> s
-                    HsSymbol s -> s 
-                CHsQVarOp -> case lexeme_HsQVarOp t of 
-                    Qual _ (HsIdent s)  -> s
-                    Qual _ (HsSymbol s) -> s
-                    UnQual (HsIdent s)  -> s
-                    UnQual (HsSymbol s) -> s
-                    -- "Special" case is always a pre-defined symbol
-                CHsConOp  -> case lexeme_HsConOp t of 
-                    HsIdent s  -> s
-                    HsSymbol s -> s 
-                CHsQConOp -> case lexeme_HsQConOp t of 
-                    Qual _ (HsIdent s)  -> s
-                    Qual _ (HsSymbol s) -> s
-                    UnQual (HsIdent s)  -> s
-                    UnQual (HsSymbol s) -> s
-                    -- "Special" case is always a pre-defined symbol
-                CHsPApp -> case lexeme_HsPApp t of 
-                    Qual _ (HsIdent s)  -> s
-                    Qual _ (HsSymbol s) -> s
-                    UnQual (HsIdent s)  -> s
-                    UnQual (HsSymbol s) -> s
-                    -- "Special" case is always a pre-defined symbol
-                CHsPInfixApp -> case lexeme_HsPInfixApp_2 t of 
-                    Qual _ (HsIdent s)  -> s
-                    Qual _ (HsSymbol s) -> s
-                    UnQual (HsIdent s)  -> s
-                    UnQual (HsSymbol s) -> s
-                    -- "Special" case is always a pre-defined symbol
-    getDecl t = case constructor t of 
-                CHsPVar -> case lexeme_HsPVar t of 
-                                HsIdent s -> s 
-                CHsConDecl -> case lexeme_HsConDecl_2 t of 
-                                HsIdent s  -> s
-                                HsSymbol s -> s 
-                CHsRecDecl -> case lexeme_HsRecDecl_2 t of 
-                                HsIdent s  -> s
-                                HsSymbol s -> s 
+                CHsVar       -> hsQName2Str $ lexeme_HsVar t
+                -- CQual        -> case getHole t of 
+                --     Just (Qual (Module x) (HsIdent y)) -> x++"."++y
+                CHsVarOp     -> hsName2Str  $ lexeme_HsVarOp t
+                CHsQVarOp    -> hsQName2Str $ lexeme_HsQVarOp t
+                CHsConOp     -> hsName2Str  $ lexeme_HsConOp t
+                CHsQConOp    -> hsQName2Str $ lexeme_HsQConOp t
+                CHsPApp      -> hsQName2Str $ lexeme_HsPApp t
+                CHsPInfixApp -> hsQName2Str $ lexeme_HsPInfixApp_2 t
+    getDecl t = case constructor t of
+                -- CHsPatBind -> case lexeme_HsPatBind_2 t of
+                --                 HsPVar (HsIdent  s) -> s
+                --                 HsPVar (HsSymbol s) -> s
+                                -- other cases we might need to say it's not a decl
+                CHsFunBind -> case head $ lexeme_HsFunBind t of -- we only take a look at the first and we assume all are equal in name
+                                HsMatch _ hsname _ _ _ -> qualify t $ hsName2Str hsname
+                CHsPVar    -> qualify t $ hsName2Str $ lexeme_HsPVar t
+                CHsConDecl -> qualify t $ hsName2Str $ lexeme_HsConDecl_2 t
+                CHsRecDecl -> qualify t $ hsName2Str $ lexeme_HsRecDecl_2 t
+                -- CHsIdent -> case constructor $ parent t of -- check if we are a name of a function
+                --                 CHsMatch -> lexeme_HsIdent t
+                --                 _ -> error "Unexpected state"
+                -- CHsSymbol -> case constructor $ parent t of -- check if we are a name of a function
+                --                 CHsMatch -> lexeme_HsSymbol t
+                --                 _ -> error "Unexpected state"
     setUse t s = case constructor t of
-                CHsVar -> case lexeme_HsVar t of 
-                            Qual x (HsIdent  a) -> setHole (HsVar (Qual x (HsIdent  $ a++s))) t
-                            Qual x (HsSymbol a) -> setHole (HsVar (Qual x (HsSymbol $ a++s))) t
-                            UnQual (HsIdent  a) -> setHole (HsVar (UnQual (HsIdent  $ a++s))) t
-                            UnQual (HsSymbol a) -> setHole (HsVar (UnQual (HsSymbol $ a++s))) t
-                CHsVarOp  -> case lexeme_HsVarOp t of
-                            HsIdent  a -> setHole (HsVarOp (HsIdent  $ a++s)) t 
-                            HsSymbol a -> setHole (HsVarOp (HsSymbol $ a++s)) t   
-                CHsQVarOp -> case lexeme_HsQVarOp t of -- infix operator
-                            Qual x (HsIdent  a) -> setHole (HsQVarOp (Qual x (HsIdent  $ a++s))) t
-                            Qual x (HsSymbol a) -> setHole (HsQVarOp (Qual x (HsSymbol $ a++s))) t
-                            UnQual (HsIdent  a) -> setHole (HsQVarOp (UnQual (HsIdent  $ a++s))) t
-                            UnQual (HsSymbol a) -> setHole (HsQVarOp (UnQual (HsSymbol $ a++s))) t
-                CHsConOp  -> case lexeme_HsConOp t of -- infix operator
-                            HsIdent  a -> setHole (HsConOp (HsIdent  $ a++s)) t 
-                            HsSymbol a -> setHole (HsConOp (HsSymbol $ a++s)) t
-                CHsQConOp -> case lexeme_HsQConOp t of -- infix operator
-                            Qual x (HsIdent  a) -> setHole (HsQConOp (Qual x (HsIdent  $ a++s))) t
-                            Qual x (HsSymbol a) -> setHole (HsQConOp (Qual x (HsSymbol $ a++s))) t
-                            UnQual (HsIdent  a) -> setHole (HsQConOp (UnQual (HsIdent  $ a++s))) t
-                            UnQual (HsSymbol a) -> setHole (HsQConOp (UnQual (HsSymbol $ a++s))) t
-                CHsPApp -> case lexeme_HsPApp t of -- application (of a constructor?)
-                            Qual x (HsIdent  a) -> setHole (HsPApp (Qual x (HsIdent  $ a++s)) (lexeme_HsPApp_2 t)) t
-                            Qual x (HsSymbol a) -> setHole (HsPApp (Qual x (HsSymbol $ a++s)) (lexeme_HsPApp_2 t)) t
-                            UnQual (HsIdent  a) -> setHole (HsPApp (UnQual (HsIdent  $ a++s)) (lexeme_HsPApp_2 t)) t
-                            UnQual (HsSymbol a) -> setHole (HsPApp (UnQual (HsSymbol $ a++s)) (lexeme_HsPApp_2 t)) t
-                CHsPInfixApp -> case lexeme_HsPInfixApp_2 t of -- infix application (of a constructor?)
-                            Qual x (HsIdent  a) -> setHole (HsPInfixApp (lexeme_HsPInfixApp t) (Qual x (HsIdent  $ a++s)) (lexeme_HsPInfixApp_3 t)) t
-                            Qual x (HsSymbol a) -> setHole (HsPInfixApp (lexeme_HsPInfixApp t) (Qual x (HsSymbol $ a++s)) (lexeme_HsPInfixApp_3 t)) t
-                            UnQual (HsIdent  a) -> setHole (HsPInfixApp (lexeme_HsPInfixApp t) (UnQual (HsIdent  $ a++s)) (lexeme_HsPInfixApp_3 t)) t
-                            UnQual (HsSymbol a) -> setHole (HsPInfixApp (lexeme_HsPInfixApp t) (UnQual (HsIdent  $ a++s)) (lexeme_HsPInfixApp_3 t)) t
-    setDecl t s = case constructor t of 
-                    CHsPVar -> case lexeme_HsPVar t of 
-                        HsIdent  a -> setHole (HsPVar $ HsIdent  $ a++s) t 
-                        HsSymbol a -> setHole (HsPVar $ HsSymbol $ a++s) t  
-                    CHsConDecl -> case lexeme_HsConDecl_2 t of 
-                        HsIdent  a -> setHole (HsConDecl (lexeme_HsConDecl t) (HsIdent  $ a++s) (lexeme_HsConDecl_3 t)) t 
-                        HsSymbol a -> setHole (HsConDecl (lexeme_HsConDecl t) (HsSymbol $ a++s) (lexeme_HsConDecl_3 t)) t  
-                    CHsRecDecl -> case lexeme_HsRecDecl_2 t of 
-                        HsIdent  a -> setHole (HsRecDecl (lexeme_HsRecDecl t) (HsIdent  $ a++s) (lexeme_HsRecDecl_3 t)) t 
-                        HsSymbol a -> setHole (HsRecDecl (lexeme_HsRecDecl t) (HsSymbol $ a++s) (lexeme_HsRecDecl_3 t)) t  
-                    
-    initialState = const ["error", "show", "$"]
+                CHsVar       -> setHole (HsVar (appendHsQNameStr (lexeme_HsVar t) s)) t
+                CHsVarOp     -> setHole (HsVarOp (appendHsNameStr (lexeme_HsVarOp t) s)) t
+                CHsQVarOp    -> setHole (HsQVarOp (appendHsQNameStr (lexeme_HsQVarOp t) s)) t -- infix operator
+                CHsConOp     -> setHole (HsConOp (appendHsNameStr (lexeme_HsConOp t) s)) t -- infix operator
+                CHsQConOp    -> setHole (HsQConOp (appendHsQNameStr (lexeme_HsQConOp t) s)) t -- infix operator
+                CHsPApp      -> setHole (HsPApp (appendHsQNameStr (lexeme_HsPApp t) s) (lexeme_HsPApp_2 t)) t -- application (of a constructor?)
+                CHsPInfixApp -> setHole (HsPInfixApp (lexeme_HsPInfixApp t) (appendHsQNameStr (lexeme_HsPInfixApp_2 t) s) (lexeme_HsPInfixApp_3 t)) t
+    setDecl t s = case constructor t of
+                    CHsPVar -> setHole (HsPVar $ appendHsNameStr (lexeme_HsPVar t) s) t
+                    CHsPatBind -> case lexeme_HsPatBind_2 t of
+                        HsPVar hsname -> setHole (HsPatBind (lexeme_HsPatBind t) (HsPVar $  appendHsNameStr hsname s) (lexeme_HsPatBind_3 t) (lexeme_HsPatBind_4 t)) t
+                                    -- other cases we might need to say it's not a decl
+                    CHsFunBind -> case head $ lexeme_HsFunBind t of -- we only take a look at the first and we assume all are equal in name
+                                    HsMatch q hsname w e r -> setHole (HsMatch q (appendHsNameStr hsname s) w e r : tail (lexeme_HsFunBind t)) t
+                    CHsConDecl -> setHole (HsConDecl (lexeme_HsConDecl t) (appendHsNameStr (lexeme_HsConDecl_2 t) s) (lexeme_HsConDecl_3 t)) t
+                    CHsRecDecl -> setHole (HsRecDecl (lexeme_HsRecDecl t) (appendHsNameStr (lexeme_HsRecDecl_2 t) s) (lexeme_HsRecDecl_3 t)) t
 
+    initialState = const names -- const ["error", "show", "$"]
 
--- TODO: constructor usage, if inside "data X =" then its decl, else its use. Check "ParseOK" in example2 
--- ^ use upwards to check if it's inside a "data=" definition <- no need, as theres a constructor just for that
--- ^^ done, without upwards
--- TODO: add infix verification (1 `otherFunc` 2)  <--- DONE / UPD: not done! distinguish decls and uses?!
--- TODO: check if we need more of HsDecls as declarations?
--- TODO: refactor - make HsQName -> String and use it! 
+-- TODO: reviews on overleaf <- DONE
+-- TODO: think about qualified names: for example (Qual Language.Haskell.Pretty pretty)
+-- TODO: change IScopes.build to allow definition of list of names, instead of definition of just 1 name, per node
+-- TODO: all decl x should declare x and also ModuleName.x (upwards until root to get module name?)
+
+-- new problem: since we declare twice, we also find errors twice!
+
+-- TODO: strategy to grab all names from prelude, into initialState
+-- TODO: properties on Haskell? What properties? 
+-- TODO: compare upwards dclblock+env vs normal env (using v)
+-- TODO: big haskell file? (last resort, let generator)
+-- TODO: can we include qualification into the Interface itself? Its a generic problem, not just haskell
+
+-----
+----- Helper functions
+-----
+
+hsName2Str :: HsName -> String
+hsName2Str (HsIdent  s) = s
+hsName2Str (HsSymbol s) = s
+
+hsQName2Str :: HsQName -> String
+hsQName2Str (Qual x hsname) = hsName2Str hsname
+hsQName2Str (UnQual hsname) = hsName2Str hsname
+
+appendHsNameStr :: HsName -> String -> HsName
+appendHsNameStr (HsIdent  a) s = HsIdent  $ a++s
+appendHsNameStr (HsSymbol a) s = HsSymbol $ a++s
+
+appendHsQNameStr :: HsQName -> String -> HsQName
+appendHsQNameStr (Qual x hsname) s = Qual x $ appendHsNameStr hsname s
+appendHsQNameStr (UnQual hsname) s = UnQual $ appendHsNameStr hsname s
+
+qualify :: Scopes a => Zipper a -> String -> [String]
+qualify t s = [s, moduleName ++ "." ++ s]
+ where (Module moduleName) = lexeme_HsModule_2 .^^ t
 
 -----
 ----- Quick Test
@@ -156,18 +161,18 @@ parseExample2 = scopesExample example2
 -----
 ----- Auxiliary
 -----
-scopesExample :: String -> String 
-scopesExample = prettyPrint . applyErrors_a68 . parse 
+scopesExample :: String -> String
+scopesExample = prettyPrint . applyErrors_a68 . parse
 
 errorsExample :: String -> Errors
 errorsExample = processor_a68 . parse
 
 blockExample :: String -> P
-blockExample = toBlock . parse 
+blockExample = toBlock . parse
 
 parse :: String -> HsModule
-parse s = case parseModule s of 
-                ParseOk x -> x 
+parse s = case parseModule s of
+                ParseOk x -> x
                 err -> error $ show err
 
 -----
@@ -184,19 +189,29 @@ example1 = "\
 \"
 
 f1 = let x = 1 + 1
-         y = 2 + x 
-         z = x 
+         y = 2 + x
+         z = x
      in z
 
 -- try adding Prelude directly so that build+env can find it
 example2 = "\
 \module Example where \n\
 \import Language.Haskell.Parser \n\
-\data Parse = ParseOke String \n\
-\parseExample = case parseModule example2 of \n\
+\data Example = ParseOk String | ParseOk Int \n\
+\ \n\ 
+\x `asdf` y = x \n\
+\x `asdf` 1 = 1 \n\
+\f = \\x y -> z \n\
+\parseExample x = let parseModule y = Language.Haskell.Parser.foo \n\
+\                     foo = parseModule 1 \n\
+\                  in case parseModule example2 of \n\
 \                ParseOk x -> x `asdf` y \n\
 \                err -> error $ show err\n\
+\ where y = 1 \n\
+\        where y = 2 \n\
 \"
+
+-- where inside where, should open new scopes
 
 example3 = "\
 \module Example where \n\
@@ -208,9 +223,57 @@ example3 = "\
 \     in z\
 \"
 
-f2 = let y = let x = 4 
-                 y = 7 
+f2 = let y = let x = 4
+                 y = 7
              in y + z
          x = 1 + 1
-         z = x 
+         z = x
      in z
+
+
+exampleModule = "\
+\module Example ( \n\
+\    Bool(False, True), \n\
+\    (&&), (||), not, otherwise, \n\
+\ \n\
+\    Maybe(Nothing, Just), \n\
+\    maybe, \n\
+\    Num((+), (-), (*), negate, abs, signum, fromInteger), \n\
+\    Real(toRational), \n\
+\ ) where \n\
+\f1 = 4 \n\
+\"
+
+instance (StrategicData (LHE.Module LHE.SrcSpanInfo))
+
+names = unsafePerformIO $ namesFromFile "./base-4.17.2.1/Prelude.hs"
+
+namesFromFile :: FilePath -> IO [String]
+namesFromFile s = do 
+    r <- LHE.parseFile s
+    return $ case r of 
+        LHE.ParseOk t -> applyTU (full_tdTU (failTU `adhocTU` oneName)) $ toZipper t 
+        _ -> ["error"] 
+
+namesFromModule :: String -> [String]
+namesFromModule s = case LHE.parseFileContents s of 
+    LHE.ParseOk t -> applyTU (full_tdTU (failTU `adhocTU` oneName)) $ toZipper t 
+    _ -> ["error"] 
+
+oneName :: LHE.ExportSpec LHE.SrcSpanInfo -> [String]
+oneName (LHE.EVar _ qname) = [qnameToStr qname]
+oneName (LHE.EAbs _ namespace qname) = [qnameToStr qname]
+oneName (LHE.EThingWith _ wildcard qname cnamelist) = qnameToStr qname : map cnameToStr cnamelist
+oneName (LHE.EModuleContents _ (LHE.ModuleName _ s)) = ["Module <s>"]
+
+qnameToStr :: LHE.QName LHE.SrcSpanInfo -> String 
+qnameToStr (LHE.Qual _ moduleName name) = nameToStr name 
+qnameToStr (LHE.UnQual _ name)          = nameToStr name 
+
+cnameToStr :: LHE.CName LHE.SrcSpanInfo -> String 
+cnameToStr (LHE.VarName _ name) = nameToStr name 
+cnameToStr (LHE.ConName _ name) = nameToStr name 
+
+nameToStr :: LHE.Name LHE.SrcSpanInfo -> String 
+nameToStr (LHE.Ident  _ s) = s 
+nameToStr (LHE.Symbol _ s) = s 
--- src/IScopes.hs
+++ src/IScopes.hs
@@ -8,7 +8,7 @@ import Data.Generics.Aliases
 import Library.ZipperAG
 import Data.Maybe
 import Debug.Trace
-import qualified Library.StrategicData as S (StrategicData(..), isJust, left, right, up, down') 
+import qualified Library.StrategicData as S (StrategicData(..), isJust, left, right, up, down')
 import qualified Block.Shared as B
 import qualified Block.Block_Zippers as BZ
 import qualified Block.SharedAG as BS
@@ -20,11 +20,11 @@ class (Typeable a, S.StrategicData a, Data a) => Scopes a where
     isUse :: Zipper a -> Bool
     isBlock :: Zipper a -> Bool
     isGlobal :: Zipper a -> Bool
-    isGlobal _ = False 
+    isGlobal _ = False
     getUse :: Zipper a -> String
     getUse a = getString a
-    getDecl :: Zipper a -> String
-    getDecl a = getString a
+    getDecl :: Zipper a -> [String]
+    getDecl a = [getString a]
     setUse :: Zipper a -> String -> Zipper a
     setUse a b = modifyFunc a b
     setDecl :: Zipper a -> String-> Zipper a
@@ -33,13 +33,14 @@ class (Typeable a, S.StrategicData a, Data a) => Scopes a where
     initialState = const []
 
 toBlock :: Scopes a => a -> B.P
-toBlock = build . toZipper 
+toBlock = build . toZipper
 
 build :: Scopes a => Zipper a -> B.P
 build a = B.Root (buildChildren build' a [])
 
 build' :: Scopes a => Zipper a -> B.Directions -> B.Its
-build' a d | isDecl a = B.ConsIts (B.Decl (getDecl a) d) (buildChildren build' a d)
+-- build' a d | isDecl a = B.ConsIts (B.Decl (getDecl a) d) (buildChildren build' a d)
+build' a d | isDecl a = mergeIts (foldr (B.ConsIts . (`B.Decl` d)) B.NilIts (getDecl a)) (buildChildren build' a d)
            | isUse a = B.ConsIts (B.Use (getUse a) d) (buildChildren build' a d)
            | isBlock a = B.ConsIts (B.Block $ buildChildren build' a d) B.NilIts
            | otherwise = buildChildren build' a d
@@ -51,71 +52,71 @@ mergeIts (B.ConsIts (B.Block B.NilIts) xs) its = mergeIts xs its
 mergeIts (B.ConsIts x xs) its = B.ConsIts x (mergeIts xs its)
 
 children :: Scopes a => (Zipper a -> B.Directions -> B.Its) -> Zipper a -> B.Directions -> [B.Its]
-children buildFunc ag d = case S.down' ag of 
+children buildFunc ag d = case S.down' ag of
   Nothing -> []
-  Just n -> do  
-    let x = d ++ [B.D] 
-    buildFunc n x : brothers buildFunc n x  
+  Just n -> do
+    let x = d ++ [B.D]
+    buildFunc n x : brothers buildFunc n x
 
 brothers :: Scopes a => (Zipper a -> B.Directions -> B.Its) -> Zipper a -> B.Directions -> [B.Its]
-brothers buildFunc ag d = case S.right ag of 
+brothers buildFunc ag d = case S.right ag of
   Nothing -> []
-  Just n -> do 
-    let x = d ++ [B.R] 
-    buildFunc n x : brothers buildFunc n x  
+  Just n -> do
+    let x = d ++ [B.R]
+    buildFunc n x : brothers buildFunc n x
 
 buildChildren :: Scopes a => (Zipper a -> B.Directions -> B.Its) -> Zipper a -> B.Directions -> B.Its
-buildChildren buildFunc ag d = foldr mergeIts B.NilIts (children buildFunc ag d) 
+buildChildren buildFunc ag d = foldr mergeIts B.NilIts (children buildFunc ag d)
 
 applyDirections :: Scopes a => Zipper a -> B.Directions -> Zipper a
 applyDirections ag [] = ag
-applyDirections ag (h:t)  | h == B.D = case S.down' ag of 
+applyDirections ag (h:t)  | h == B.D = case S.down' ag of
                                         Nothing -> error "Can't go there!"
-                                        Just n -> applyDirections n t 
-                          | h == B.R = case S.right ag of 
+                                        Just n -> applyDirections n t
+                          | h == B.R = case S.right ag of
                                         Nothing -> error "Can't go there!"
-                                        Just n -> applyDirections n t 
-                          | h == B.L = case S.left ag of 
+                                        Just n -> applyDirections n t
+                          | h == B.L = case S.left ag of
                                         Nothing -> error "Can't go there!"
-                                        Just n -> applyDirections n t 
-                          | h == B.U = case S.up ag of 
+                                        Just n -> applyDirections n t
+                          | h == B.U = case S.up ag of
                                         Nothing -> error "Can't go there!"
-                                        Just n -> applyDirections n t 
+                                        Just n -> applyDirections n t
 
 modifyZipperAlongPath :: Scopes a => Zipper a -> (BS.Name, BS.It, String) -> Zipper a
 modifyZipperAlongPath ag (_, B.Use   _ d, e)  = setUse  (applyDirections ag d) e
 modifyZipperAlongPath ag (_, B.Decl  _ d, e)  = setDecl (applyDirections ag d) e
 
 modifyFunc :: Scopes a => Zipper a -> String -> Zipper a
-modifyFunc ag s = case down' ag of 
+modifyFunc ag s = case down' ag of
   Nothing -> error "Error going down on modifyFunc"
   Just n -> case (getHole n :: Maybe String) of
                 Just holeString -> setHole (holeString ++ s) n
                 Nothing -> aux n s
 
 aux :: Scopes a => Zipper a -> String -> Zipper a
-aux ag s = case right ag of 
+aux ag s = case right ag of
   Nothing -> error "Error going right on modifyFunc"
   Just n -> case (getHole n :: Maybe String) of
                 Just holeString -> setHole (holeString ++ s) n
                 Nothing -> aux n s
 
 getString :: Scopes a => Zipper a -> String
-getString ag = case down' ag of 
+getString ag = case down' ag of
   Nothing -> error "Error going down on getString"
   Just n -> case (getHole n :: Maybe String) of
                 Just holeString -> holeString
                 Nothing -> getString' n
 
 getString' :: Scopes a => Zipper a -> String
-getString' ag = case right ag of 
+getString' ag = case right ag of
   Nothing -> error "Error going right on getString"
   Just n -> case (getHole n :: Maybe String) of
                 Just holeString -> holeString
                 Nothing -> getString' n
 
 applyErrors :: Scopes a => Zipper a -> B.Errors -> Zipper a
-applyErrors ag [] = ag 
+applyErrors ag [] = ag
 applyErrors ag (e: t) = applyErrors (mkAG $ fromZipper $ modifyZipperAlongPath ag e) t
 
 string2Env :: [String] -> B.Env
